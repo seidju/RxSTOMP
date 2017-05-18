85d4be430ee71c095360ffb39dcf7f51c48c9580
Pavel Shatalov <shatalovp@gmail.com>
2017-05-18 09:58:20 +0700

    Initial Commit

---
+++ RxSTOMP/RxSTOMP.h
@@ -0,0 +1,19 @@
+//
+//  RxSTOMP.h
+//  RxSTOMP
+//
+//  Created by Pavel Shatalov on 18.05.17.
+//  Copyright © 2017 Pavel Shatalov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxSTOMP.
+FOUNDATION_EXPORT double RxSTOMPVersionNumber;
+
+//! Project version string for RxSTOMP.
+FOUNDATION_EXPORT const unsigned char RxSTOMPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSTOMP/PublicHeader.h>
+
+
